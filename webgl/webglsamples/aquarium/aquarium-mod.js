018a7ac8318a563ecd13575820f213d35434434c
Yang Gu <yang.gu@intel.com>
2017-05-19 18:53:09 +0800

    Aquarium: Change modelViewMatrix to viewMatrix

--- webgl/webglsamples/aquarium/aquarium-mod.js
+++ webgl/webglsamples/aquarium/aquarium-mod.js
@@ -1103,7 +1103,7 @@ function initialize() {
     setCanvasSize(canvas, g.globals.width, g.globals.height);
   }
 
-  function render(elapsedTime, projectionMatrix, modelViewMatrix) {
+  function render(elapsedTime, projectionMatrix, viewMatrix) {
     /*
     var now = theClock.getTime();
     var elapsedTime;
@@ -1181,7 +1181,7 @@ function initialize() {
     var yOff = height * g.net.offset[1] * g.net.offsetMult;
     if (g_vrDisplay && g_vrDisplay.isPresenting) {
       fast.matrix4.copy(projection, projectionMatrix);
-      fast.matrix4.inverse(viewInverse, modelViewMatrix);
+      fast.matrix4.inverse(viewInverse, viewMatrix);
     } else {
       fast.matrix4.frustum(
         projection,
